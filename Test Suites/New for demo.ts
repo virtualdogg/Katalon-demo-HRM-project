74e47e61f45127fec6f426ac4a10927565256ac8
arawat <arawat@PQA-0532LT.pqatesting.com>
2024-05-22 16:09:32 -0400

    cxz

---
+++ Test Suites/New for demo.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New for demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e8fdf008-8184-415f-8773-2f3f09aa9f7b</testSuiteGuid>
+</TestSuiteEntity>
