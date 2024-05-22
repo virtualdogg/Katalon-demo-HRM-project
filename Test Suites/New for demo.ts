eea3014895041e1742745bee6814985f83e614a2
arawat <arawat@PQA-0532LT.pqatesting.com>
2024-05-22 16:59:35 -0400

    cs

--- Test Suites/New for demo.ts
+++ Test Suites/New for demo.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e8fdf008-8184-415f-8773-2f3f09aa9f7b</testSuiteGuid>
+   <testCaseLink>
+      <guid>74fd6dcf-d0dc-4c2f-8c36-60a7e5cd07fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
